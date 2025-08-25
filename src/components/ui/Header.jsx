72c24d8c0c07b82e9ed94c5370e6fcb85878ad88
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-08-25 16:27:06 -0300

    Create Header component

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <img src="/src/assets/images/logo.png" alt="IMC logo" />
+      <h1>Calculadora de Índice de Massa Corporal</h1>
+    </header>
+  );
+}
+
+export default Header;

21beeeaa8f4429615bee70f317352d2e41576a23
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-08-26 01:03:29 -0300

    Styling update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/ui/Header";
+import Form from "./components/content/Form";
 
 function App() {
   return (
     <main>
       <Header />
+      <Form />
     </main>
   );
 }

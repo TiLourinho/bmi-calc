32ea17573778f54fcca0381e56fca4708662ad4c
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-08-27 18:27:27 -0300

    Update `helpers.jsx` file

--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,5 +1,17 @@
-export default function convertCentimetersToMeters(number) {
+import classificationInfo from "../assets/data/classification";
+
+export function convertCentimetersToMeters(number) {
   if (!number.includes(".")) number /= 100;
 
   return number;
+}
+
+export function classifyResult(index) {
+  if (index < 18.5) return classificationInfo[0];
+  if (index < 25.0) return classificationInfo[1];
+  if (index < 30.0) return classificationInfo[2];
+  if (index < 35.0) return classificationInfo[3];
+  if (index < 40.0) return classificationInfo[4];
+
+  return classificationInfo[5];
 }
